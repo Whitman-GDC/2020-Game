b1982d84b500c654fe8edd2498b64c62726116a6
Yixuan Li <lxd520ANNA@gmail.com>
2020-12-06 09:05:52 -0500

    add player movement, stealth movement, (crappy but working) pick up and drop mechanics

---
+++ 2020 Game/Assets/Scripts/PickUp.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PickUp : MonoBehaviour
+{
+
+    public LayerMask interactables;
+    public float maxInteractableDistance = 15f;
+    public float dropUpwardForce = 4f, dropForwardForce = 3f;
+
+    public GameObject interactText;
+    public GameObject canvas;
+
+    public Transform pickUpContainer;
+	public Transform environment;
+    public Transform player;
+
+	GameObject holding;
+    Vector3 holdingOriginalScale;
+
+    void Awake()
+    {
+        interactText.SetActive(false);
+        interactText.GetComponent<Text>().text = "Press F to pick up";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Ray ray = gameObject.GetComponent<Camera>().ScreenPointToRay(Input.mousePosition);
+		if (Physics.Raycast(ray, out RaycastHit hit, maxInteractableDistance, interactables))
+		{
+            // display the hint when raycast hits and interactable object
+			interactText.SetActive(true);
+            if (Input.GetButton("PickUp"))
+			{
+                if (holding)
+                {
+					// if player is already holding something, drop it first
+					Drop();
+				}
+                // pick up when the player presses the pickup key
+                PickUpObject(hit.collider.gameObject);
+            }
+		}
+		else
+		{
+			interactText.SetActive(false);
+		}
+
+        if (Input.GetButton("Drop"))
+		{
+            if (holding)
+            {
+                Drop();
+            }
+		}
+
+        if (holding)
+		{
+            holding.transform.localPosition = Vector3.zero;
+            holding.transform.localRotation = Quaternion.Euler(Vector3.zero);
+            holding.transform.localScale = Vector3.one;
+        }
+	}
+
+    void PickUpObject(GameObject item)
+	{
+        // set current holding item to object picked up
+        holding = item;
+        holdingOriginalScale = item.transform.localScale;
+
+		// make the item a child of the pickup container and set it to default position
+		holding.transform.SetParent(pickUpContainer);
+        holding.transform.localPosition = Vector3.zero;
+        holding.transform.localRotation = Quaternion.Euler(Vector3.zero);
+        holding.transform.localScale = Vector3.one;
+    }
+
+    void Drop()
+    {
+        // setting parent to null
+        holding.transform.SetParent(environment);
+
+        // add forces to throw away item
+        holding.GetComponent<Rigidbody>().AddForce(transform.forward * dropForwardForce, ForceMode.Impulse);
+        holding.GetComponent<Rigidbody>().AddForce(transform.up * dropUpwardForce, ForceMode.Impulse);
+
+        holding.transform.localScale = holdingOriginalScale;
+        holding = null;
+    }
+}
--- 2020 Game/Assets/Scripts/PlayerMovement.cs
+++ 2020 Game/Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,51 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
+    public CharacterController controller;
+
+    public float moveSpeed = 12f;
+    public float stealthSpeedMultiplier = 0.5f;
+
+    public float gravity = -9.81f;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
+    Vector3 velocity;
+    bool isGrounded;
+
     void Update()
     {
-        
+
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if (isGrounded && velocity.y < 0)
+		{
+            velocity.y = -2f;
+		}
+
+        float movementX = Input.GetAxis("Horizontal");
+        float movementZ = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * movementX + transform.forward * movementZ;
+
+        float finalMoveSpeed = moveSpeed;
+
+        if (Input.GetButton("Stealth"))
+		{
+            finalMoveSpeed *= stealthSpeedMultiplier;
+		}
+		else
+		{
+            finalMoveSpeed = moveSpeed;
+		}
+
+        controller.Move(move * finalMoveSpeed * Time.deltaTime);
+
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
     }
 }
