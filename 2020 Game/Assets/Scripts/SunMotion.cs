eecaf91fb3473224f7e453f129116b351f141951
M-A-T-T-11 <m4tt_0@icloud.com>
2020-12-08 16:34:04 -0500

    Added basic day night cycle motion

---
+++ 2020 Game/Assets/Scripts/SunMotion.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+public class SunMotion : MonoBehaviour
+{
+    public float radius;
+    public float rateOfTime;
+    private float time;     
+    private float currentTime;
+    // private float duskDawnTime;
+
+    private void Start()
+    {
+        radius = 800f;
+        rateOfTime = 1f;
+        time = 1000f;            // Day cycles every 1000 update when rate = 1
+        // duskDawnTime = 50f;
+        currentTime = time;
+        transform.position = new Vector3(0, radius, 0);
+    }
+
+    private void FixedUpdate()
+    {
+        transform.position = new Vector3(radius * Mathf.Cos((currentTime/time) * 2 * Mathf.PI), 
+                                            radius * Mathf.Sin((currentTime/time) * 2 * Mathf.PI), 0f);
+        transform.LookAt(new Vector3(0,0,0));
+        
+        /*
+        if(time == 0)  
+        {
+            // Start dusk
+        } 
+        else if(time >= duskDawnTime) 
+        {
+            // End dusk
+        }
+        else if(time >= time/2-duskDawnTime)
+        {
+            // Start dawn
+        }
+        */
+
+        if(currentTime > time)
+        {
+            currentTime = 0f;
+        } else {
+            currentTime += rateOfTime;
+        }
+    }
+
+}
