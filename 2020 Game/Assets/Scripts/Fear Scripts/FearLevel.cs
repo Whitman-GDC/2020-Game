0b365375fe4474804ae125560f1ee350fd2bb58a
samtroost <75699239+samtroost@users.noreply.github.com>
2020-12-20 16:48:45 -0500

    Added the scripts for the fear mechanic

---
+++ 2020 Game/Assets/Scripts/Fear Scripts/FearLevel.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class FearLevel : MonoBehaviour
+{
+    public Slider fearslider;
+    public float startfear;
+    public static float currentfear;
+
+
+    void Start()
+    {
+        currentfear = startfear;
+    }
+
+    void Update()
+    {
+        fearslider.value = currentfear;
+    }
+}
