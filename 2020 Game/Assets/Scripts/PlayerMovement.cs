4005852539d9f59a902fd3e0cde671b32cf9d77e
Ethan Ma <31868061+ethanma03@users.noreply.github.com>
2021-01-12 16:33:16 -0500

    Added Rigidbody movement to the player, jumping still needed
    I commented lots of stuff out in the code, pls delete what is unimportant tyvm

--- 2020 Game/Assets/Scripts/PlayerMovement.cs
+++ 2020 Game/Assets/Scripts/PlayerMovement.cs
@@ -1,17 +1,18 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-
+    private Rigidbody rb;
     public CharacterController controller;
 
     public float moveSpeed = 12f;
     public float stealthSpeedMultiplier = 0.5f;
 
     public float gravity = -9.81f;
-
+    public float jumpForce;
     public Transform groundCheck;
     public float groundDistance = 0.4f;
     public LayerMask groundMask;
@@ -19,21 +20,42 @@ public class PlayerMovement : MonoBehaviour
     Vector3 velocity;
     bool isGrounded;
 
-    void FixedUpdate()
+    private void Start()
     {
-
+        rb = GetComponent<Rigidbody>();
+    }
+    void Update()
+    {
+        
         isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
 
+        /*
         if (isGrounded && velocity.y < 0)
 		{
             velocity.y = -2f;
 		}
+        */
+        
+        //input
+        float movementX = Input.GetAxisRaw("Horizontal") * moveSpeed;
+        float movementZ = Input.GetAxisRaw("Vertical") * moveSpeed;
 
-        float movementX = Input.GetAxis("Horizontal");
-        float movementZ = Input.GetAxis("Vertical");
-
+        //movement
         Vector3 move = transform.right * movementX + transform.forward * movementZ;
+        Vector3 newMove = new Vector3(move.x, rb.velocity.y, move.z);
 
+        rb.velocity = newMove;
+        
+        if(isGrounded)
+        {
+            Console.WriteLine("is grounded");
+        }
+        //jumping
+        if(Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.velocity = new Vector3(rb.velocity.x, jumpForce, rb.velocity.z);
+        }
+        /*
         float finalMoveSpeed = moveSpeed;
 
         if (Input.GetButton("Stealth"))
@@ -50,5 +72,6 @@ public class PlayerMovement : MonoBehaviour
         velocity.y += gravity * Time.deltaTime;
 
         controller.Move(velocity * Time.deltaTime);
+        */
     }
 }
