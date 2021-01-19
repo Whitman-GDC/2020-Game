1cf87b579f70b9b745a9576dc89bf5374fc15647
M-A-T-T-11 <m4tt_0@icloud.com>
2021-01-19 16:00:22 -0500

    Fixed syntax errors

--- 2020 Game/Assets/Scripts/RoomLoader.cs
+++ 2020 Game/Assets/Scripts/RoomLoader.cs
@@ -16,19 +16,19 @@ public class RoomLoader : MonoBehaviour
         */
         anchorPoint = new Vector3(0, 0, 0);     // The coordinate where the next room should go
         activeRooms = new List<GameObject>();               
-        activeRooms.add(Instantiate(getNextRoom(), anchorPoint, Quaternion.identity));
+        activeRooms.Add(Instantiate(getNextRoom(), anchorPoint, Quaternion.identity));
         for(int i = 1; i < depth; i++) 
         {
-            addNewRoom()
+            addNewRoom();
         }
     }
 
-    void OnTriggerEnter(Collision col) 
+    void OnTriggerEnter(Collider col) 
     {   
         // The room that collides with player is the next room (middle)
         if(activeRooms.IndexOf(col.gameObject) == ((depth-1)/2+1)) 
         {
-            addNewRoom()
+            addNewRoom();
             activeRooms.RemoveAt(0);
         }
     }
@@ -38,13 +38,13 @@ public class RoomLoader : MonoBehaviour
         GameObject nextRoom = getNextRoom();        // Two empty game objects 0 is entrance 1 is exit
         Transform startPoint = nextRoom.transform.GetChild(0).gameObject.GetComponent<Transform>();
         Transform endPoint = activeRooms[activeRooms.Count-1].transform.GetChild(1).gameObject.GetComponent<Transform>();
-        anchorPoint = anchorPoint + endPoint.position + (-startPoint);
-        activeRooms.add(Instantiate(nextRoom, anchorPoint, Quaternion.identity));   // Currently assuming all rooms are lined up
+        anchorPoint = anchorPoint + endPoint.position + (-startPoint.position);
+        activeRooms.Add(Instantiate(nextRoom, anchorPoint, Quaternion.identity));   // Currently assuming all rooms are lined up
     }
 
     private GameObject getNextRoom() 
     {
-        int randomRoom = Random.range(0, rooms.Length-1);
+        int randomRoom = Random.Range(0, rooms.Length-1);
         return rooms[randomRoom];
     }
 
