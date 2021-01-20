25fbe9f637fdffd38d32d6fff709449b2d6a4531
Maxohm491 <55296645+Maxohm491@users.noreply.github.com>
2021-01-20 07:38:29 -0700

    created animation
    created animation of the spider

---
+++ 2020 Game/Assets/Scripts/TakeSteps.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TakeSteps : MonoBehaviour
+{
+    public Transform target;
+    public float stepLength;
+    public float speed = 1;
+    public float marginOfError;
+    private Transform transform;
+    private Vector3 nextTarget;  
+    private bool takingStep = false;
+    
+
+    void Start()
+    {
+        transform = GetComponent<Transform>();
+        nextTarget = transform.position;
+    }
+
+    void Update()
+    {
+        //This is the best way I found to smoothly move the base of the leg to the target. Transform.Translate is too fast.
+        if((Math.Abs((target.position-transform.position).magnitude) >= stepLength) && !takingStep) {
+            takingStep = true; 
+            nextTarget = target.position;
+        }
+
+        if(takingStep) {
+            float step = speed * Time.deltaTime;
+            transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+        }
+
+        if(takingStep && Math.Abs((transform.position-nextTarget).magnitude) <=marginOfError)
+        {
+            takingStep = false;
+        }
+
+        Debug.Log(takingStep);
+    }
+}
